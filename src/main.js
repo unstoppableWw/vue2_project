899834cd5c502529f009034f67ad6c724dbc01a2
unstoppableWw <190559573@qq.com>
2023-12-11 10:41:38 +0800

    增加了axios和mock

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "./style/global.less";
 import VueRouter from "vue-router";
 import router from "@/router";
 import showMessage from "./utils/showMessage";
+import "./mock";
+import { getBanners } from "./api/banner";
+getBanners()
 Vue.prototype.$showMessage = showMessage;
 Vue.use(VueRouter);
 new Vue({
--- src/utils/showMessage.js
+++ src/utils/showMessage.js
@@ -1,6 +1,6 @@
-import getComponentRootDom from "./getComponentRootDom";
-import Icon from "@/components/Icon";
-import styles from "./showMessage.module.less";
+import getComponentRootDom from './getComponentRootDom'
+import Icon from '@/components/Icon'
+import styles from './showMessage.module.less'
 
 /**
  * 弹出消息
@@ -9,46 +9,48 @@ import styles from "./showMessage.module.less";
  * @param {Number} duration 多久后消失
  * @param {HTMLElement} container 容器，消息会显示到该容器的正中；如果不传，则显示到页面正中
  */
-export default function(options = {}) {
-  const content = options.content || "";
-  const type = options.type || "info";
-  const duration = options.duration || 2000;
-  const container = options.container || document.body;
+export default function (options = {}) {
+  const content = options.content || ''
+  const type = options.type || 'info'
+  const duration = options.duration || 2000
+  const container = options.container || document.body
   // 创建消息元素
-  const div = document.createElement("div");
+  const div = document.createElement('div')
   const iconDom = getComponentRootDom(Icon, {
-    type,
-  });
-  div.innerHTML = `<span class="${styles.icon}">${iconDom.outerHTML}</span><div>${content}</div>`;
+    type
+  })
+  div.innerHTML = `<span class="${styles.icon}">${iconDom.outerHTML}</span><div>${content}</div>`
   // 设置样式
-  const typeClassName = styles[`message-${type}`]; //类型样式名
-  div.className = `${styles.message} ${typeClassName}`;
+  const typeClassName = styles[`message-${type}`] //类型样式名
+  div.className = `${styles.message} ${typeClassName}`
   // 将div加入到容器中
-
-  // 容器的position是否改动过
-  if (getComputedStyle(container).position === "static") {
-    container.style.position = "relative";
+  if (options.container) {
+    // 容器的position是否改动过
+    if (getComputedStyle(container).position === 'static') {
+      container.style.position = 'relative'
+    }
   }
-  container.appendChild(div);
+
+  container.appendChild(div)
   // 浏览器强行渲染
-  div.clientHeight; // 导致reflow
+  div.clientHeight // 导致reflow
 
   // 回归到正常位置
-  div.style.opacity = 1;
-  div.style.transform = `translate(-50%, -50%)`;
+  div.style.opacity = 1
+  div.style.transform = `translate(-50%, -50%)`
 
   // 等一段时间，消失
   setTimeout(() => {
-    div.style.opacity = 0;
-    div.style.transform = `translate(-50%, -50%) translateY(-25px)`;
+    div.style.opacity = 0
+    div.style.transform = `translate(-50%, -50%) translateY(-25px)`
     div.addEventListener(
-      "transitionend",
-      function() {
-        div.remove();
+      'transitionend',
+      function () {
+        div.remove()
         // 运行回调函数
-        options.callback && options.callback();
+        options.callback && options.callback()
       },
       { once: true }
-    );
-  }, duration);
+    )
+  }, duration)
 }
