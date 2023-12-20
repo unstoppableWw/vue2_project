6f2a1594797bd084a4026d010f0833445d0b9be5
unstoppableWw <190559573@qq.com>
2023-12-20 15:41:08 +0800

    完成留言板

--- src/Views/Project.vue
+++ src/Views/Project.vue
@@ -1,31 +1,18 @@
 <template>
   <div class="project-container" ref="projectContainer" v-loading="loading">
     <div v-for="item in data" :key="item.id" class="project-item">
-      <a
-        :href="
-          item.url ? item.url : `javascript:alert('该项目无法通过外网访问')`
-        "
-        :target="item.url ? '_blank' : '_self'"
-      >
+      <a :href="item.url ? item.url : `javascript:alert('该项目无法通过外网访问')`
+        " :target="item.url ? '_blank' : '_self'">
         <img class="thumb" v-lazy="item.thumb" />
       </a>
       <div class="info">
         <h2>
-          <a
-            :href="
-              item.url ? item.url : `javascript:alert('该项目无法通过外网访问')`
-            "
-            :target="item.url ? '_blank' : '_self'"
-          >
+          <a :href="item.url ? item.url : `javascript:alert('该项目无法通过外网访问')`
+            " :target="item.url ? '_blank' : '_self'">
             {{ item.name }}
           </a>
         </h2>
-        <a
-          class="github"
-          target="_blank"
-          :href="item.github"
-          v-if="item.github"
-        >
+        <a class="github" target="_blank" :href="item.github" v-if="item.github">
           github
         </a>
         <p v-for="(desc, i) in item.description" :key="i">
@@ -50,6 +37,7 @@ export default {
 
 <style lang="less" scoped>
 @import "~@/style/var.less";
+
 .project-container {
   width: 100%;
   height: 100%;
@@ -59,16 +47,19 @@ export default {
   overflow-y: auto;
   scroll-behavior: smooth;
 }
+
 .project-item {
   transition: 0.5s;
   padding: 20px;
   display: flex;
   margin-bottom: 20px;
+
   &:hover {
     box-shadow: -1px 1px 5px #000;
     transform: scale(1.01) translate(3px, -3px);
     color: inherit;
   }
+
   .thumb {
     width: 250px;
     min-height: 150px;
@@ -77,13 +68,16 @@ export default {
     border-radius: 5px;
     margin-right: 15px;
   }
+
   .info {
     line-height: 1.7;
     flex: 1 1 auto;
+
     h2 {
       margin: 0;
     }
   }
+
   .github {
     font-size: 14px;
     color: @primary;
